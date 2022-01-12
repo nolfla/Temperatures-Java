a44d02926011599ad24a75f8a263ee91b474e206
nolfla <44060336+nolfla@users.noreply.github.com>
2022-01-12 09:56:25 -0700

    Create Temperatures.java

---
+++ Temperatures.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+ 
+public class Temperatures {
+ 
+  public static void main(String[] args) {
+    
+    ArrayList<Integer> temperatures = new ArrayList<Integer>();
+    
+    temperatures.add(78);
+    temperatures.add(67);
+    temperatures.add(89);
+    temperatures.add(94);
+    
+    System.out.println(temperatures.get(2));
+    
+  }
+}
